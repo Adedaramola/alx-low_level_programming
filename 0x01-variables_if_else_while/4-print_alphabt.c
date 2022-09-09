9daa82905341f5c1e541072c7b0bba3268c9d591
Adedaramola <mailstokindle@gmail.com>
2022-09-09 07:40:20 -0700

    alphabet soup

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,10 +10,14 @@ int main(void)
 	int lower = 97;
 
 	while (lower < 123)
+	{
 		putchar(lower);
 		if (lower == 113 || lower == 101)
+		{
 			continue;
+		}
 		lower++;
+	}
 	putchar(10);
 	return (0);
 }
