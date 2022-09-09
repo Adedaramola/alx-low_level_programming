c783a1197a998e0e6f1a03ceb30ed44a31643da8
Adedaramola <mailstokindle@gmail.com>
2022-09-09 14:41:31 -0700

    alphabet soup

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+		printf("%d", num);
+	printf("\n");
+	return (0);
+}
