8a580150f358376bcceec9a203f28649a6526b5f
Adedaramola <mailstokindle@gmail.com>
2022-09-09 15:38:13 -0700

    persistence

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,15 +7,16 @@
  */
 int main(void)
 {
-	int lower = 97;
+	int lower = 96;
 
 	while (lower < 123)
 	{
+		lower++;
 		if (lower == 101 || lower == 113)
 		{
 			continue;
 		}
-		putchar(lower++);
+		putchar(lower);
 	}
 	putchar(10);
 	return (0);
