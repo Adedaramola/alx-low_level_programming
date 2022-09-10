c0050d222cdc1049e127cc4b90126b10086200ce
Adedaramola <mailstokindle@gmail.com>
2022-09-09 19:15:48 -0700

    zazu

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - print doubles
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a, b, c, d;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = '0'; b <= '9'; b++)
+		{
+			for (c = a; c <= '9'; c++)
+			{
+				for (d = b + 1; d <= '9'; d++)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(' ');
+					putchar(c);
+					putchar(d);
+					if (!((a == '9' && b == '8') && (c == '9' && d == '9)))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				d = '0';
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
