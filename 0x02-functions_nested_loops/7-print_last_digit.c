d79b33a715e7f1685d85d9188bf5e731077f674e
Adedaramola <mailstokindle@gmail.com>
2022-09-13 05:38:02 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit
+ * @num: integer to be tested
+ * Return: 0
+ */
+int print_last_digit(int num)
+{
+	int last_digit;
+
+	last_digit = num % 10;
+	return (last_digit);
+}
