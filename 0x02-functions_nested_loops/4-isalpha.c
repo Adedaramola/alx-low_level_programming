e7f6811936745319981efebba2bd4b133aa0ae5d
Adedaramola <mailstokindle@gmail.com>
2022-09-13 04:40:22 -0700

    pretty quick

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /** 
- * _islower - checks for lowercase
+ * _isalpha - checks for alpha
  *
  * Return: 0 or 1
  */
-int _islower(int c)
+int _ialpha(int c)
 {
-	if (c >= 97 && c <= 122)
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
