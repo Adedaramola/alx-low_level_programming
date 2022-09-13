8714a554adacad15fae607401f8d49b19380135b
Adedaramola <mailstokindle@gmail.com>
2022-09-13 04:18:38 -0700

    nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,15 +7,16 @@
  */
 void print_alphabet_x10(void)
 {
-	int count = 0;
-	int start = 97;
+	int count = 1;
+	int start;
 
-	while (count++ <= 9)
+	while (count <= 10)
 	{
-		while (start <= 122)
+		for (start = 97; start <= 122; start++)
 		{
-			_putchar(start++);
+			_putchar(start);
 		}
+		count++;
 	}
 	_putchar(10);
 }
